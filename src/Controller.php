d307b2f66e55773ec9d934530fbfad42aa016fe6
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2023-11-05 11:39:09 +0100

    Refactor: fixed the views

--- src/Controller.php
+++ src/Controller.php
@@ -2,16 +2,47 @@
 
 namespace MVC;
 
-class Controller {
-    protected function render($view, $data = []) {
-        extract($data);
+class Controller
+{
+
+    static protected $directory = __DIR__ . '/Views/';
+    static protected $base_view = __DIR__ . '/Views/base.phtml';
+    protected $view;
+    protected $partial;
+    protected $data;
+
+
+    protected function render($view, $data = [])
+    {
+
+        $this->view = static::$directory . "$view.phtml";
+        $this->data = $data;
         
-        include __DIR__ . "/Views/$view.phtml";
+        extract($data);
+
+        include static::$base_view;
     }
 
-    public function notfound($data) {     
+    public function notfound($data)
+    {
         extract($data);
-        
-        include __DIR__ . "/Views/404.phtml";
+
+        include static::$directory . "404.phtml";
+    }
+
+    public function include($partial, $data = array())
+    {
+        $partial = static::$directory . $partial;
+        $this->partial = is_null($partial) || !file_exists($partial) ? static::$directory . '_blank.phtml' : $partial;
+
+        extract($data);
+
+        include $this->partial;
+    }
+
+    public function content()
+    {
+        extract($this->data);
+        include $this->view;
     }
 }
