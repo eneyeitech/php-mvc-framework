09151875d6ae53387474e32a13354b7919615008
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2023-11-05 20:07:00 +0100

    Refactored: fixed tittle bug

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -7,19 +7,10 @@ use MVC\Helpers\Config;
 
 class HomeController extends Controller
 {
-    //static protected $config_path = __DIR__ . '/../../config';
-    //protected $siteDetails;
-
-    function __construct()
-    {
-        //Config::setDirectory(static::$config_path);
-        //$this->siteDetails = Config::get('site');
-        
-    }
 
     public function index()
     {
-        $this->siteDetails['vPage'] = 'people';
+        $this->siteDetails['vPage'] = 'index';
         $this->siteDetails['title'] = 'Academic Staff';
         $this->render('home/index', $this->siteDetails);
     }
