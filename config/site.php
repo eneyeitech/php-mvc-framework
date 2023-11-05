cf46fbeac242bd94f53276781df639279c49e316
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2023-11-05 13:48:07 +0100

    Added config file site.php

---
+++ config/site.php
@@ -0,0 +1,44 @@
+<?php 
+
+return [
+    'site'=>'department',
+    'headTitle'=>'Head of Department',
+    'title' => 'Department of Chemical Engineering.',
+    'deptName'=>'Department of Chemical Engineering',
+    'facName'=>'Home',
+    'facUrl'=>'https://engineering.abu.edu.ng',
+    'instName'=>'Ahmadu Bello University, Zaria - Nigeria',
+    'head_name'=>'Prof. A. Hamza',
+    'head_img_url'=>'assets/images/hod/prof-a-hamza.jpg',
+    'abu_mission'=>'The Mechanical Engineering Program at ABU makes available a high quality, relevant engineering education by providing students with a set of skills, knowledge and attitudes that will permit its graduates to succeed and thrive as engineers and leaders. The program stives to: ',
+    'welcome_message'=>'<p style="text-align: justify;">The Department was established in 1973 initially as the Industrial Chemistry Section of the Chemistry Department in the Faculty of Science. Even though the course structure was very strongly Chemical Engineering, the department was only formally converted to the Department of Chemical Engineering and transferred to the Faculty of Engineering in October 1976. The Department was established with the assistance of the University of Twente, Enschede, the Netherlands under a ten-year (1972-1982) cooperation program sponsored by the Dutch Government. A link program exists between us in the form of training support from the Netherlands till date.<br>
+    The department has now developed to such a stage where the undergraduate program has been firmly established. The first set of bachelors graduates were produced in July 1977. The department runs a four year (post advanced level) degree program from inception till present. However, in compliance with the Federal government’s new educational structure, the department introduced a five-year (post senior secondary school), course credit and semester system at the beginning of the 1988/89 academic year.<br>
+    The departmental post-graduate program based only on research work, started in 1981. Although research activities covered various areas of chemical engineering, a general Master of Science degree in Chemical Engineering was awarded to successful candidates. In the 1985/86 session, the program was expanded to include coursework in addition to the research component. Correspondingly, the award of degrees in two broad fields of process engineering and polymer/ceramic engineering was proposed. Furthermore, in the 1987/88 session, the administration of a masters program into which, allied, but non-chemical engineering bachelors degree holders were admitted, was initiated in parallel with the existing program. The irregular students are required to successfully complete prescribed packages of deficiencies in their bachelors’ degree with respect to the chemical engineering bachelors’ degree program prior to commencing the regular programme. In order to meet the yearning for training by individual and the industries around us, the department commenced Postgraduate Diploma Programme in Chemical Engineering in 2007/2008 academic session. The post-graduate program leading to the award of Doctor of Philosophy (PhD) in Chemical Engineering started in the year 1987.</p>',
+    'history'=>'<p style="text-align: justify;"> 
+
+    The Department was established in 1973 initially as the Industrial Chemistry Section of the Chemistry Department in the Faculty of Science. Even though the course structure was very strongly Chemical Engineering, the department was only formally converted to the Department of Chemical Engineering and transferred to the Faculty of Engineering in October 1976. The Department was established with the assistance of the University of Twente, Enschede, the Netherlands under a ten-year (1972-1982) cooperation program sponsored by the Dutch Government. A link program exists between us in the form of training support from the Netherlands till date.<br>
+The department has now developed to such a stage where the undergraduate program has been firmly established. The first set of bachelors graduates were produced in July 1977. The department runs a four year (post advanced level) degree program from inception till present. However, in compliance with the Federal government’s new educational structure, the department introduced a five-year (post senior secondary school), course credit and semester system at the beginning of the 1988/89 academic year.<br>
+Prior to the commencement of the undergraduate academic program in October 1973, an ‘off-campus’ post-graduate-program which was primarily meant to enhance staff training for the department, had commenced in 1972. The coursework and research were administered in the Department of Chemical Engineering in the Netherlands while the degrees were awarded by the Ahmadu Bello University. The products of this program, prior to the change of name of the department were accordingly awarded degrees in Industrial Chemistry.<br>
+The departmental post-graduate program based only on research work, started in 1981. Although research activities covered various areas of chemical engineering, a general Master of Science degree in Chemical Engineering was awarded to successful candidates. In the 1985/86 session, the program was expanded to include coursework in addition to the research component. Correspondingly, the award of degrees in two broad fields of process engineering and polymer/ceramic engineering was proposed. Furthermore, in the 1987/88 session, the administration of a masters program into which, allied, but non-chemical engineering bachelors degree holders were admitted, was initiated in parallel with the existing program. The irregular students are required to successfully complete prescribed packages of deficiencies in their bachelors’ degree with respect to the chemical engineering bachelors’ degree program prior to commencing the regular programme. In order to meet the yearning for training by individual and the industries around us, the department commenced Postgraduate Diploma Programme in Chemical Engineering in 2007/2008 academic session. The post-graduate program leading to the award of Doctor of Philosophy (PhD) in Chemical Engineering started in the year 1987.
+    <br>
+<h3>Mission</h3>
+<p>The mission of our department is to educate and train undergraduate and postgraduate students in Chemical Engineering who can effectively contribute in the growth of the field of chemical engineering in Nigeria, Africa and the World; to contribute knowledge in the area of Chemical Engineering through conducting research targeted at solving societal and industrial problems; and provide community service through consultancy, enterprenureship and research projects.</p>
+
+<h3>Vision</h3>
+<p>The Chemical Engineering Department shall be the bedrock of the growth and development of Chemical Process Industry in Nigeria through the training of highly skilled chemical engineers at both undergraduate and postgraduate levels.</p>
+
+<h3>Philosophy</h3>
+<p>To educate and train highly skilled chemical engineers that will contribute to national and global technological development.
+To be the bedrock of the growth and development of Chemical Process Industry (CPI) in Nigeria, with the capacity to contribute to sustainable economic growth of the country and place it amongst the best in the comity of nations.</p>
+
+</p>',
+    
+        
+    
+    'contact'=>[
+        'phone'=>'',
+        'address'=>'Department of Chemical Engineering,<br> Ahmadu Bello University, Samaru Main Campus,<br> Zaria - Nigeria.<br> <br> <b>HOD</b><br>Dr. Jaju Abubakar Muhammad<br>chemegg@abu.edu.ng<br> +234 803 614 4603<br><br><b>Examination Officer</b><br>Dr. Abdulkarim Abubakar<br><br>+234 806 695 1137<br><br> <b>Secretary</b><br>Mrs. Victoria Bola Omotosho<br>+234 908 269 7516',
+        'email'=>'',
+    ],
+    'view_path' => realpath('./views')
+];
--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -3,11 +3,24 @@
 namespace MVC\Controllers;
 
 use MVC\Controller;
+use MVC\Helpers\Config;
 
-class HomeController extends Controller {
-    public function index(){ 
-        $this->render('home/index');
+class HomeController extends Controller
+{
+    static protected $config_path = __DIR__ . '/../../config';
+    protected $siteDetails;
+
+    function __construct()
+    {
+        Config::setDirectory(static::$config_path);
+        $this->siteDetails = Config::get('site');
+        
     }
 
-    
+    public function index()
+    {
+        $this->siteDetails['vPage'] = 'people';
+        $this->siteDetails['title'] = 'Academic Staff';
+        $this->render('home/index', $this->siteDetails);
+    }
 }
